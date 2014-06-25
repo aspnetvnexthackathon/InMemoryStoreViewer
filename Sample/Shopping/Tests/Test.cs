cbc97978f565e5cc9e9c659c431241f4d8b8fd6b
Suhas Joshi <suhasbjoshi@hotmail.com>
2014-06-25 15:52:15 -0700

    Completed unit test scenario

--- Sample/Shopping/Tests/Test.cs
+++ Sample/Shopping/Tests/Test.cs
@@ -1,16 +1,63 @@
-﻿using Moq;
+﻿using Microsoft.Data.Entity;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.DependencyInjection.Fallback;
+using Moq;
 using System;
 using WebApp.Controllers;
+using WebApp.Models;
 using Xunit;
 
 namespace Tests
 {
     public class Test
     {
+        private IServiceProvider serviceProvider;
+
+        public Test()
+        {
+            SetupServices();
+        }
+
         [Fact]
         public void ControllerDataViewTest()
         {
-            var ctrl = new Mock<HomeController>();
+
+            var controller = new HomeController(serviceProvider.GetService<CustomersDbContext>());
+           // var result = controller.Index();
+
+            AppAssertEqual(() => Assert.Equal("foo", "bar"));
+        }
+
+        private void SetupServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddInMemoryStoreViewer();
+            services.AddEntityFramework().AddInMemoryStore();
+            services.AddTransient<CustomersDbContext>();
+
+            // Add MVC services to the services container
+            services.AddMvc();
+
+            serviceProvider = services.BuildServiceProvider();
+
+            // Create sample data
+            CustomersDbContext.CreateSampleData(serviceProvider);
+        }
+
+        private void AppAssertEqual(Action action)
+        {
+            try
+            {
+                action();
+
+            } catch(Exception e)
+            {
+                var dumpWriter = serviceProvider.GetService<InMemoryStoreViewer.InMemoryDatabaseDumper>();
+                dumpWriter.DumpDatabaseToFile(@"C:\Users\sujosh\Desktop\memdump.txt");
+
+            }
         }
     }
+
 }
