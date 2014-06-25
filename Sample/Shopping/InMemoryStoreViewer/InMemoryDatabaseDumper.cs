1379b52c74776f912f40d7ec791bca44edce5311
Maurycy Markowski <maumar@microsoft.com>
2014-06-25 15:00:39 -0700

    added dumper

---
+++ Sample/Shopping/InMemoryStoreViewer/InMemoryDatabaseDumper.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace InMemoryStoreViewer
+{
+
+    public class InMemoryDatabaseDumper
+    {
+        public static void DumpDatabaseToFile(IServiceProvider services, string path)
+        {
+            var inMemoryInfo = new InMemoryInformation(services, new ProjectInformationRetriever(services));
+            var jsonString = JsonConvert.SerializeObject(inMemoryInfo.GetInMemoryInformation());
+            File.WriteAllText(path, jsonString);
+        }
+    }
+}
