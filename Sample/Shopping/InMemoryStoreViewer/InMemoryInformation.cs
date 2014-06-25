aac0aafcc633d367a4c7bff5440e783ac3ceaeb7
Suhas Joshi <suhasbjoshi@hotmail.com>
2014-06-25 12:58:58 -0700

    getting list of Dbcontexts

--- Sample/Shopping/InMemoryStoreViewer/InMemoryInformation.cs
+++ Sample/Shopping/InMemoryStoreViewer/InMemoryInformation.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace InMemoryStoreViewer
 {
@@ -6,15 +10,62 @@ namespace InMemoryStoreViewer
     {
         public IServiceProvider Services { get; set; }
 
-        public InMemoryInformation(IServiceProvider services)
+        public ProjectInformationRetriever InformationRetriever { get; set; }
+
+        public InMemoryInformation(IServiceProvider services, ProjectInformationRetriever informationRetriever)
         {
             Services = services;
+            InformationRetriever = informationRetriever;
         }
 
         public InMemoryStore GetInMemoryInformation()
         {
+            var types = InformationRetriever.GetDbContexts();
+
             return null;
         }
 
+    }
+
+    public class ProjectInformationRetriever
+    {
+        public IServiceProvider AppServices { get; set; }
+
+        public ProjectInformationRetriever(IServiceProvider services)
+        {
+            AppServices = services;
+        }
+
+        /// <summary>
+        /// Get list of Dbcontext files in the application
+        /// </summary>
+        /// <returns></returns>
+        public IList GetDbContexts()
+        {
+            var classList = new ArrayList();
+
+            foreach (var assemblyName in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                try
+                {
+                    Assembly assembly = Assembly.Load(assemblyName.FullName);
+
+                    foreach (var classType in assembly.GetTypes())
+                    {
+                        if (classType.BaseType != null && classType.BaseType.Equals(typeof(DbContext)))
+                        {
+                            var contxt = AppServices.GetService(classType);
+
+                            classList.Add(contxt);
+                        }
+                    }
+                } catch(Exception e)
+                {
+                    continue;
+                }
+            }
+
+            return classList;
+        }
     }
 }
---
+++ Sample/Shopping/WebApp/Controllers/DataViewer.cs
@@ -0,0 +1,26 @@
+﻿using InMemoryStoreViewer;
+using Microsoft.AspNet.Mvc;
+using System;
+
+namespace WebApp
+{
+    /// <summary>
+    /// Summary description for DataViewer
+    /// </summary>
+    public class DataViewerController :Controller
+    {
+        public InMemoryInformation Information { get; set; }
+
+        public DataViewerController(InMemoryInformation information)
+	    {
+            Information = information;
+	    }
+
+        public IActionResult Index()
+        {
+            var str = Information.GetInMemoryInformation();
+
+            return View();
+        }
+    }
+}
